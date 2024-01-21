3d259734f8359a2c6e6239bb4d1d650ef1e400ed
samiragalarov <samiraglarov0@gmail.com>
2024-01-21 20:44:26 +0100

    hope

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS  # Import the CORS module
-# from src.pipeline.predict_pipeline import generate_translation
+from src.pipeline.predict_pipeline import generate_translation
 import os
 import sys
-# from src.exception import CustomException
-# from src.logger import logging
+from src.exception import CustomException
+from src.logger import logging
 
 
 application = Flask(__name__)
@@ -13,20 +13,21 @@ CORS(app)  # Enable CORS for all routes
 
 application
 
+@app.route('/') 
 def index():
     return render_template('index.html')
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    # user_input = request.args.get('input', '')
-    # data = generate_translation(50, source_sentence=(user_input,))
-    # print(data[0][:10])
-    # print(type(str(data)))
-    ###
+    user_input = request.args.get('input', '')
+    data = generate_translation(50, source_sentence=(user_input,))
+    print(data[0][:10])
+    print(type(str(data)))
+    
 
 
-    # return jsonify(str(data[0][:10]))
-    return 'ds'
+    return jsonify(str(data[0][:10]))
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
