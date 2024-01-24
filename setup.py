f512e14bec11728760bf2f1e8d9be4d66f6bf9c8
samiragalarov <samiraglarov0@gmail.com>
2024-01-24 11:17:59 +0100

    jhbytv

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
-import subprocess
 
+HYPEN_E_DOT = '-e .'
 
-
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
-
-
-    requirements=[]
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-    
+
     return requirements
 
 setup(
-name='Transformer',
-version='0.0.1',
-author='Samir',
-author_email='samiraglarov0@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
-
+    name='Transformer',
+    version='0.0.1',
+    author='Samir',
+    author_email='samiraglarov0@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    dependency_links=['--no-cache-dir'],
 )
