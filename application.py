72235c40bd6fc264802d034a17b41086e9931064
samiragalarov <samiraglarov0@gmail.com>
2024-01-24 10:59:06 +0100

    jnthj

--- application.py
+++ application.py
@@ -5,13 +5,7 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
-import subprocess
 
-index_url = "https://download.pytorch.org/whl/cpu"
-package = "torch"
-
-command = f"pip3 install {package} --index-url {index_url}"
-subprocess.run(command, shell=True)
 
 application = Flask(__name__)
 app = application
--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import find_packages,setup
 from typing import List
+import subprocess
+
+
 
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
+
+
+    index_url = "https://download.pytorch.org/whl/cpu"
+    package = "torch"
+
+    command = f"pip3 install {package} --index-url {index_url}"
+    subprocess.run(command, shell=True)
     '''
     this function will return the list of requirements
     '''
@@ -25,43 +35,3 @@ packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
 )
-# from setuptools import find_packages, setup
-# from setuptools.command.install import install
-# from typing import List
-# import subprocess
-
-# HYPEN_E_DOT = '-e .'
-
-
-# class CustomInstallCommand(install):
-#     def run(self):
-#         # Run the custom command before the actual installation
-#         subprocess.run(['TMPDIR=/home/user/tmp/ python3 -m pip install torch'], shell=True)
-#         # Continue with the default installation
-#         install.run(self)
-
-
-# def get_requirements(file_path: str) -> List[str]:
-#     '''
-#     this function will return the list of requirements
-#     '''
-#     requirements = []
-#     with open(file_path) as file_obj:
-#         requirements = file_obj.readlines()
-#         requirements = [req.replace("\n", "") for req in requirements]
-
-#         if HYPEN_E_DOT in requirements:
-#             requirements.remove(HYPEN_E_DOT)
-
-#     return requirements
-
-
-# setup(
-#     name='Transformer',
-#     version='0.0.1',
-#     author='Samir',
-#     author_email='samiraglarov0@gmail.com',
-#     packages=find_packages(),
-#     install_requires=get_requirements('requirements.txt'),
-#     cmdclass={'install': CustomInstallCommand}
-# )
